c973c969c545986375a122397ec929be349818d0
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-05-05 10:08:49 +0500

    home page

--- lanapp/models.py
+++ lanapp/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Info(models.Model):
+    class Meta:
+        verbose_name = "Info"
+        verbose_name_plural = "Malumotlar"
+
+
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     text = models.TextField()
     
     def __str__(self):
         return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse("lanapp:info", kwargs={'slug': self.slug})
