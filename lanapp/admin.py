82d03f38b1915d5a399f33fcb544c9caf8108a9f
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-05-07 16:25:09 +0500

    translationmodel

--- lanapp/admin.py
+++ lanapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Info
+from modeltranslation.admin import TranslationAdmin
 
 @admin.register(Info)
-class InfoAdmin(admin.ModelAdmin):
+class InfoAdmin(TranslationAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
